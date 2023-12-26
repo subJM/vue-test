fcd4958a8fc7962c4c82c3bf097280e197e6841c
손종민 <thswhdals@banco.id>
2023-12-26 10:49:49 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
 <template>
+  <NavBar/>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NavBar
   }
 }
 </script>
 
 <style>
+body{
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/NavBar.vue
@@ -0,0 +1,66 @@
+<template>
+    <header>
+        <article class="nav" >
+          <div class="title" style="width:200px;"><a href="./index.html"><font-awesome-icon :icon="['fas', 'house']" />BANCO</a></div>
+          <div id="menu" >
+    
+          </div>
+          <div >
+            <a href="./index.html">HOME</a>
+          </div>
+          <div>
+            <router-link to="/about">About 페이지로 이동</router-link>
+            <!-- <a href="./index.html">ABOUT</a> -->
+          </div>
+          <div>
+            <a href="./index.html">NOTICE</a>
+          </div>
+          <div>
+            <a href="./index.html">BUSINESS</a>
+          </div>
+          <div>
+            <a href="./index.html">CONTACT</a>
+          </div>
+        
+          <div style="width:200px;">
+            <font-awesome-icon icon="fa-brands fa-square-twitter" style="width:50px;height:50px;" />
+          </div>
+
+        </article>
+    </header>
+</template>
+
+<script></script>
+
+<style>
+div a {
+  text-decoration: none;
+  color:white;
+}
+
+li {
+  list-style-type: none;
+}
+
+.nav {
+  background-color: black;
+  width: 100%;
+  height: 80px;
+}
+
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 10px;
+  color: white;
+}
+
+.title {
+  font-size: 2em;
+}
+
+.title > a {
+  color: white;
+}
+</style>
